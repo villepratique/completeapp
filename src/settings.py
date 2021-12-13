93883ba69c2f7838e4d9aacae4a7c8829102ffef
Richard-Cod <richard.bathiebo.7@gmail.com>
2021-12-13 19:13:37 +0100

    e

--- src/settings.py
+++ src/settings.py
@@ -120,13 +120,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),)
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "generator/static",
-#     BASE_DIR / "booking/static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "generator/static",
+    BASE_DIR / "booking/static",
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
