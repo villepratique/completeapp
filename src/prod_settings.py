0673fb9b0ba3df61818f5c75521c5259a47a6207
Richard-Cod <richard.bathiebo.7@gmail.com>
2022-01-18 09:57:10 +0100

    e

--- src/prod_settings.py
+++ src/prod_settings.py
@@ -1,6 +1,6 @@
 from src.settings import *
 DEBUG = True
-ALLOWED_HOSTS = ["pyproject99.herokuapp.com" , "mavillepratique.fr"]
+ALLOWED_HOSTS = ["pyproject99.herokuapp.com" , "mavillepratique.fr" , "www.mavillepratique.fr"]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 SITE_URL="https://pyproject99.herokuapp.com"
